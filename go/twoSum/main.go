a366cd7883f4f5eb5940b1199d89ea18020fe396
Juan Vieira <juanvieira1@yahoo.com>
2026-01-05 16:20:34 -0300

    perf: improve performance reducing to complexity from O(n²) to O(n)
    Switching: from slices to maps

--- go/twoSum/main.go
+++ go/twoSum/main.go
@@ -1,12 +1,15 @@
 package main
 
-import "twoSum/solution"
+import (
+	"fmt"
+	"twoSum/solution"
+)
 
 func main() {
 	testCase1 := solution.TwoSum([]int{2, 7, 11, 15}, 9)
 	testCase2 := solution.TwoSum([]int{3, 2, 4}, 6)
 	testCase3 := solution.TwoSum([]int{3, 3}, 6)
-	println(testCase1)
-	println(testCase2)
-	println(testCase3)
+	fmt.Println(testCase1)
+	fmt.Println(testCase2)
+	fmt.Println(testCase3)
 }
--- go/twoSum/solution/twoSum.go
+++ go/twoSum/solution/twoSum.go
@@ -1,20 +1,13 @@
 package solution
 
 func TwoSum(nums []int, target int) []int {
-	var output []int
-
-	for num := 0; num < len(nums) - 1; num++ {
-		for num2 := num +1; num2 < len(nums); num2++ {
-			if num == num2 {
-				continue
-			}
-
-			if nums[num] + nums[num2] == target {
-				output = append(output, num, num2)
-				return output
-			}
+	hashmap := make(map[int]int)
+	for i, n := range nums {
+		complement := target - n
+		if val, ok := hashmap[complement]; ok {
+			return []int{val, i}
 		}
+		hashmap[n] = i
 	}
-
 	return []int{}
 }
