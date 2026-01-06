ec9eb48bd183f78004f3ef546023e6a69c4af49e
Juan Vieira <juanvieira1@yahoo.com>
2026-01-06 10:24:18 -0300

    refactor: rename function

--- python/palindrome/main.py
+++ python/palindrome/main.py
@@ -1,9 +1,9 @@
 from palindrome import Solution
 
 solution = Solution()
-isPalindrome = solution.isPalindrome_2(x=121)
-isPalindrome2 = solution.isPalindrome_2(x=-121)
-isPalindrome3 = solution.isPalindrome_2(x=10)
+isPalindrome = solution.isPalindrome_mathApproach(x=121)
+isPalindrome2 = solution.isPalindrome_mathApproach(x=-121)
+isPalindrome3 = solution.isPalindrome_mathApproach(x=10)
 print(isPalindrome)
 print(isPalindrome2)
 print(isPalindrome3)
