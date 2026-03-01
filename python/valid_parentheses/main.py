480de5b83a0d4a686bf1ee5dcb8f6d5148e126cd
Juan Vieira <juanvieira1@yahoo.com>
2026-02-28 21:13:03 -0300

    feat: implement valid parentheses in python

---
+++ python/valid_parentheses/main.py
@@ -0,0 +1,10 @@
+from valid_parentheses import Solution
+
+solution = Solution()
+
+print(solution.isValid("()"))
+print(solution.isValid("()[]{}"))
+print(solution.isValid("(]"))
+print(solution.isValid("([])"))
+print(solution.isValid("([)]"))
+print(solution.isValid("["))
